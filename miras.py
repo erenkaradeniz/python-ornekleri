7298fa39baff3fc018feeac093faf55c4667db17
Eren Karadeniz <erenkaradeniz@users.noreply.github.com>
2017-12-31 20:24:05 +0300

    Add files via upload

---
+++ miras.py
@@ -0,0 +1,46 @@
+"""
+# Miras alma
+* Çoklu mirasa destek veriyor
+* class sınıf_ismi(ust sınıf ismi)
+* class sınıf_ismi(ust1, ust2, ...)
+"""
+
+# Kisi sınıfı
+# Ogrenci(Kisi) Sınıfı
+# Ogretmen(Kisi) Sınıfı
+
+class Kisi:
+	""" Kişiyi temsil eden sınıftır. """
+	def __init__(self, isim, yas):
+		self.isim = isim
+		self.yas = yas
+
+	def listele(self):
+		print("isim : ", self.isim, " yas : ", self.yas)
+
+class Ogrenci(Kisi):
+	def __init__(self, isim, yas, numara):
+		# Üst sınıfa bilgiler gönderiliyor.
+		Kisi.__init__(self, isim, yas)
+		self.numara = numara
+
+	def listele(self):
+		# isim ve yas kısımlarının yazdırılması
+		Kisi.listele(self)
+		print("numara : ", self.numara)
+
+class Ogretmen(Kisi):
+	def __init__(self, isim, yas, brans):
+		# üst sınıfın kurucu metodunu çağırılıyor
+		Kisi.__init__(self, isim, yas)
+		self.brans = brans
+
+	def listele(self):
+		Kisi.listele(self)
+		print("brans : ", self.brans)
+
+o1 = Ogrenci("Eren", "22", 2013)
+k = Ogretmen("Gürcan", "33", "Bilgisayar")
+
+o1.listele()
+k.listele()
